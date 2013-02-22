70a82dff4325bed3e00788ab47c268c1f17389f6
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2013-02-22 12:51:36 -0300

    Code fixes and improvements.

--- tasks_application/tasks/admin.py
+++ tasks_application/tasks/admin.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 *-*
 from models import Task
 from models import Project
 from django.contrib import admin
 
+
 class TaskAdmin(admin.ModelAdmin):
-    radio_fields= {"project": admin.VERTICAL}
+    radio_fields = {"project": admin.VERTICAL}
 
 admin.site.register(Task, TaskAdmin)
 admin.site.register(Project)
--- tasks_application/tasks/views.py
+++ tasks_application/tasks/views.py
@@ -1,29 +1,28 @@
+# -*- coding: utf-8 *-*
+from datetime import datetime
+
+from django.contrib import messages
+#from django.contrib.auth.views import redirect_to_login
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import Task
-from models import Project
-
-from forms import TaskForm
-from forms import ProjectForm
-from django.core.urlresolvers import reverse
-
-from django.views.generic.create_update import get_model_and_form_class, lookup_object
-from django.views.generic.create_update import redirect
 from django.utils.translation import ugettext
-from django.contrib import messages
+from django.views.generic.create_update import (get_model_and_form_class,
+                                                lookup_object, redirect)
 
-from django.contrib.auth.decorators import login_required
+from tasks.forms import (TaskForm, ProjectForm)
+from tasks.models import (Task, Project)
 
-from datetime import datetime
 
 def index(request):
     r = {}
     return render_to_response('tasks/home.html', r, RequestContext(request))
 
+
 def project_set(request, object_id):
     try:
-        obj = Project.objects.get(pk = object_id)
+        obj = Project.objects.get(pk=object_id)
         obj.last_access = datetime.now()
         request.session['current_project'] = obj
         obj.save()
@@ -31,89 +30,103 @@ def project_set(request, object_id):
         request.session['current_project'] = None
         pass
     return HttpResponseRedirect(reverse('tasks_home'))
-    
+
+
 def process(request):
     r = {}
-    r['inbox_list'] = Task.objects.filter(size = 0)
+    r['inbox_list'] = Task.objects.filter(size=0)
     inbox_first = None
     if r['inbox_list']:
         inbox_first = r['inbox_list'][0]
-    r['form'] = TaskForm(instance = inbox_first)
+    r['form'] = TaskForm(instance=inbox_first)
     r['inbox_first'] = inbox_first
     if inbox_first:
-        return render_to_response('tasks/process.html', r, RequestContext(request))
+        return render_to_response('tasks/process.html', r,
+                                  RequestContext(request))
     else:
         return HttpResponseRedirect(reverse('tasks_home'))
+
 
 def task_duplicate(request, object_id):
     obj = Task.objects.duplicate(object_id)
     if obj:
-        return HttpResponseRedirect(reverse('tasks_task_edit', kwargs={'object_id': obj.id}))
+        return HttpResponseRedirect(
+                    reverse('tasks_task_edit', kwargs={'object_id': obj.id}))
     else:
         return HttpResponseRedirect(reverse('tasks_home'))
-    
+
+
 def task_detail(request, object_id):
     try:
         r = {}
-        r['object'] = Task.objects.get(pk = object_id)
+        r['object'] = Task.objects.get(pk=object_id)
     except:
         pass
-    return render_to_response('tasks/task_detail.html', r, RequestContext(request))
-    
+    return render_to_response('tasks/task_detail.html', r,
+                              RequestContext(request))
+
+
 def project_close(request, object_id):
     try:
-        obj = Project.objects.get(pk = object_id)
+        obj = Project.objects.get(pk=object_id)
         if obj.is_closed:
             obj.is_closed = False
         else:
-            obj.is_closed = True 
+            obj.is_closed = True
         obj.save()
-        if obj  ==  request.session.get('current_project'):
-            return project_set(request, object_id = 0)
+        if obj == request.session.get('current_project'):
+            return project_set(request, object_id=0)
     except:
         pass
     return HttpResponseRedirect(reverse('tasks_project_general'))
 
+
 def project_general(request):
     r = {}
     r['form'] = ProjectForm()
-    r['project_list'] = Project.objects.filter(owner = request.user)
-    return render_to_response('tasks/project_general.html', r, RequestContext(request))
+    r['project_list'] = Project.objects.filter(owner=request.user)
+    return render_to_response('tasks/project_general.html', r,
+                              RequestContext(request))
 
-def create_user_owned_object(request, 
-                             model = None,template_name = None,
-                             extra_context = None, post_save_redirect = None,
-                             login_required = False, context_processors = None, 
-                             form_class = None):
-    if extra_context is None: extra_context = {}
+
+def create_user_owned_object(request,
+                             model=None, template_name=None,
+                             extra_context=None, post_save_redirect=None,
+                             login_required=False, context_processors=None,
+                             form_class=None):
+    if extra_context is None:
+        extra_context = {}
     if login_required and not request.user.is_authenticated():
         return redirect_to_login(request.path)
 
     model, form_class = get_model_and_form_class(model, form_class)
-    if request.method  ==  'POST':
+    if request.method == 'POST':
         form = form_class(request.POST, request.FILES)
         if form.is_valid():
             #~ new_object = Task()
-            new_object = form.save(commit = False)
+            new_object = form.save(commit=False)
             #~ relation with session user
             new_object.owner = request.user
             new_object.save()
 
             msg = ugettext("The %(verbose_name)s was created successfully.") %\
                                     {"verbose_name": model._meta.verbose_name}
-            messages.success(request, msg, fail_silently = True)
+            messages.success(request, msg, fail_silently=True)
             return redirect(post_save_redirect, new_object)
     else:
         form = form_class()
 
     # Create the template, context, response
     if not template_name:
-        template_name = "%s/%s_form.html" % (model._meta.app_label, model._meta.object_name.lower())
-        
-    return render_to_response(template_name, {'form': form}, RequestContext(request))
+        template_name = "%s/%s_form.html" % (model._meta.app_label,
+                                             model._meta.object_name.lower())
+
+    return render_to_response(template_name, {'form': form},
+                              RequestContext(request))
 
     if not template_name:
-        template_name = "%s/%s_form.html" % (model._meta.app_label, model._meta.object_name.lower())
+        template_name = "%s/%s_form.html" % (model._meta.app_label,
+                                             model._meta.object_name.lower())
     t = template_loader.get_template(template_name)
     c = RequestContext(request, {
         'form': form,
@@ -121,16 +134,19 @@ def create_user_owned_object(request,
     }, context_processors)
     apply_extra_context(extra_context, c)
     response = HttpResponse(t.render(c))
-    populate_xheaders(request, response, model, getattr(obj, obj._meta.pk.attname))
+    populate_xheaders(request, response, model,
+                      getattr(obj, obj._meta.pk.attname))
     return response
 
-def task_update(request, model = None, 
-                object_id = None, slug = None,
-                slug_field = 'slug', template_name = None,
-                extra_context = None, post_save_redirect = None, 
-                login_required = False, context_processors = None, 
-                template_object_name = 'object',form_class = None):
-    if extra_context is None: extra_context = {}
+
+def task_update(request, model=None,
+                object_id=None, slug=None,
+                slug_field='slug', template_name=None,
+                extra_context=None, post_save_redirect=None,
+                login_required=False, context_processors=None,
+                template_object_name='object', form_class=None):
+    if extra_context is None:
+        extra_context = {}
     if login_required and not request.user.is_authenticated():
         return redirect_to_login(request.path)
     model, form_class = get_model_and_form_class(model, form_class)
@@ -138,93 +154,113 @@ def task_update(request, model = None,
 
     if request.method == 'POST':
         #~ recuperar o crear el proyecto
-        p = Project.objects.get_or_create_by_owner_and_title(request.user,request.POST.get('project_title'))
-        form = form_class(request.POST, request.FILES, instance = obj)
+        p = Project.objects.get_or_create_by_owner_and_title(
+                               request.user, request.POST.get('project_title'))
+        form = form_class(request.POST, request.FILES, instance=obj)
         del form.fields['project']
         if form.is_valid():
-            obj = form.save(commit = False)
+            obj = form.save(commit=False)
             obj.project = p
             obj.save()
             msg = ugettext("The %(verbose_name)s was updated successfully.") %\
                                     {"verbose_name": model._meta.verbose_name}
-            messages.success(request, msg, fail_silently = True)
+            messages.success(request, msg, fail_silently=True)
             if "_continue" not in request.POST:
                 return redirect(post_save_redirect, obj)
     else:
-        form = form_class(instance = obj)
+        form = form_class(instance=obj)
     if not template_name:
-        template_name = "%s/%s_form.html" % (model._meta.app_label, model._meta.object_name.lower())
-    return render_to_response(template_name, {'form': form,'object':obj}, RequestContext(request))
+        template_name = "%s/%s_form.html" % (
+                        model._meta.app_label, model._meta.object_name.lower())
+    return render_to_response(template_name, {'form': form, 'object': obj},
+                              RequestContext(request))
 
-def task_create(request, model = None,  
-                template_name = None, extra_context = None, 
-                post_save_redirect = None, login_required = False, 
-                context_processors = None, form_class = None):
-    if extra_context is None: extra_context = {}
+
+def task_create(request, model=None,
+                template_name=None, extra_context=None,
+                post_save_redirect=None, login_required=False,
+                context_processors=None, form_class=None):
+
+    if extra_context is None:
+        extra_context = {}
     if login_required and not request.user.is_authenticated():
         return redirect_to_login(request.path)
 
     model, form_class = get_model_and_form_class(model, form_class)
-    if request.method  ==  'POST':
-        p = Project.objects.get_or_create_by_owner_and_title(request.user,request.POST.get('project_title_inbox'))
+    if request.method == 'POST':
+        p = Project.objects.get_or_create_by_owner_and_title(
+                        request.user, request.POST.get('project_title_inbox'))
         form = form_class(request.POST, request.FILES)
         if form.is_valid():
-            new_object = form.save(commit = False)
+            new_object = form.save(commit=False)
             new_object.project = p
             new_object.save()
             msg = ugettext("The %(verbose_name)s was created successfully.") %\
                                     {"verbose_name": model._meta.verbose_name}
-            messages.success(request, msg, fail_silently = True)
+            messages.success(request, msg, fail_silently=True)
             return redirect(post_save_redirect, new_object)
     else:
         form = form_class()
     if not template_name:
-        template_name = "%s/%s_insert_form.html" % (model._meta.app_label, model._meta.object_name.lower())
-    return render_to_response(template_name, {'form': form}, RequestContext(request))
+        template_name = "%s/%s_insert_form.html" % (
+                        model._meta.app_label, model._meta.object_name.lower())
+    return render_to_response(template_name, {'form': form},
+                              RequestContext(request))
+
 
 def project_json(request):
     r = {}
-    if request.method  ==  'GET':
+    if request.method == 'GET':
         q = request.GET.get('q')
         #~ from django.core import serializers
         #~ data = serializers.serialize("json", )
-        r['object_list'] = Project.objects.filter(owner = request.user).filter(is_closed = False).filter(title__contains = q)
+        r['object_list'] = Project.objects.filter(owner=request.user).filter(
+                                     is_closed=False).filter(title__contains=q)
         pass
-    return render_to_response('tasks/project_json.html', r, RequestContext(request))
+    return render_to_response('tasks/project_json.html', r,
+                              RequestContext(request))
+
 
 def task_json(request):
     r = {}
-    if request.method  ==  'GET':
+    if request.method == 'GET':
         q = request.GET.get('q')
-        project_list = Project.objects.filter(owner = request.user)
-        pre_list = Task.objects.filter(project__in = project_list)
-        pre_list1 = pre_list.filter(title__contains = q)
-        pre_list2 = pre_list.filter(detail__contains = q)
+        project_list = Project.objects.filter(owner=request.user)
+        pre_list = Task.objects.filter(project__in=project_list)
+        pre_list1 = pre_list.filter(title__contains=q)
+        pre_list2 = pre_list.filter(detail__contains=q)
         #~ r['object_list'] = Task.objects.filter(title__contains = q)
         r['object_list'] = pre_list1 | pre_list2
         # filtrar por detail
         pass
-    return render_to_response('tasks/task_json.html', r, RequestContext(request))
+    return render_to_response('tasks/task_json.html', r,
+                              RequestContext(request))
+
 
 def filter_highlighted(request, value):
     request.session['filter_highlighted'] = value
     return HttpResponseRedirect(reverse('tasks_home'))
 
+
 def task_toggle_block(request, object_id):
     Task.objects.toggle_block(object_id)
     return HttpResponseRedirect(reverse('tasks_home'))
-    
+
+
 def task_toggle_highlight(request, object_id):
     Task.objects.toggle_highlight(object_id)
     return HttpResponseRedirect(reverse('tasks_home'))
 
+
 def task_toggle_delay(request, object_id):
     Task.objects.toggle_delay(object_id)
     return HttpResponseRedirect(reverse('tasks_home'))
-    
+
+
 def task_toggle_archived(request, object_id):
     obj = Task.objects.toggle_archived(object_id)
     if obj:
-        if obj.is_archived == False:
-            return HttpResponseRedirect(reverse('tasks_task_edit', kwargs={'object_id': obj.id}))
+        if not obj.is_archived:
+            return HttpResponseRedirect(reverse('tasks_task_edit',
+                                                kwargs={'object_id': obj.id}))
     return HttpResponseRedirect(reverse('tasks_home'))
